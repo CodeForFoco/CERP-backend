7d24472a5fcaee7ae8e7991249595ab468d0e098
Matthew Harris <matt@annuitycheck.com>
2017-10-21 19:53:22 -0600

    cleaned up data.py file to match pep8, reduced number of variables declared for single use

--- cerp/data.py
+++ cerp/data.py
@@ -8,30 +8,32 @@ import pandas as pd
 def presidential_election_16():
     """
         This returns the presidental data for the 2016 election.
-        Currently it creates then destroys a csv object, this
-        step should be removed
-    """
-    with open("cerp/static/2016-PresidentialElection.json") as f:
-        data = json.load(f)
+        Specifically a dataframe that looks like:
 
-    # Extract proper level
-    a = data['precincts']
+                     canadit | canadit   | ...
+        precinctNum   votes  |   votes   | ...
+        precinctNum   votes  |   votes   | ...
+        ...
+    """
+    with open("cerp/static/2016-PresidentialElection.json") as file_handler:
+        data = json.load(file_handler)
 
     # Convert to DataFrame and select columns we care about
-    b = pd.DataFrame(a)
-    c = b[['precinctNumber', 'votes']]
+    data_frame = pd.DataFrame(data['precincts'])
+    data_frame = data_frame[['precinctNumber', 'votes']]
 
     # Iterate through dataframe to create list with proper associations
     result = []
-    for idx, row in c.iterrows():
+    for _, row in data_frame.iterrows():
         for dct in row['votes']:
             dct['precinctNumber'] = row['precinctNumber']
             result.append(dct)
 
     # Convert back to dataframe and pivot to what you wanted
-    df = pd.DataFrame(result)
-    df = df.pivot(index='precinctNumber', columns='candidate', values='votes')
-    return df
+    return pd.DataFrame(result).pivot(
+        index='precinctNumber',
+        columns='candidate',
+        values='votes')
 
 
 # Load all
