69e5bc3882b9f78a6372774eeb53cec526b4d8b7
Matthew Harris <943559+deidyomega@users.noreply.github.com>
2017-10-26 02:59:11 -0600

    fixed coverage.sh

--- coverage.sh
+++ coverage.sh
@@ -1,5 +1,8 @@
 coverage run --omit=venv/* --source=cerp -m unittest discover tests/
-if [ $1 != "--automated" ]; then
+
+if [ $# -eq 1 ] && [ $1 = "--automated" ]; then
+echo 'ignoring run'
+else
 coverage html
 xdg-open htmlcov/index.html
 fi
