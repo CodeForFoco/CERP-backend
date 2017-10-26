9278d5cd4de91e9306a3bab8fe77f44aaf83ce91
Matthew Harris <943559+deidyomega@users.noreply.github.com>
2017-10-26 02:44:21 -0600

    trying to automate with coveralls

--- coverage.sh
+++ coverage.sh
@@ -1,6 +1,6 @@
-rm .coverage
-rm -r htmlcov
 coverage run --omit=venv/* --source=cerp -m unittest discover tests/
+if [ $1 != "--automated" ]; then
 coverage html
 xdg-open htmlcov/index.html
 coverage-badge -o coverage.svg
+fi
