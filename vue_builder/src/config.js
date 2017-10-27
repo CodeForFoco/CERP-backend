09212d2cdf54e122364be809c9012dd69fea185c
Jordan Majd <jordan@madtitanstudios.com>
2017-10-26 20:13:13 -0600

    Remove front end from repo

--- vue_builder/src/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  API_LOCATION: 'http://localhost:5000' // working on both python and vue
-  // API_LOCATION: '' // production
-  // API_LOCATION: 'https://cerp-code-foco.herokuapp.com' // working on vue, but don't want to run python server
-}
