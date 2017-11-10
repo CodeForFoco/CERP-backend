e2157a66eb84942eb2f8f5211df94f3f726c3361
Brad Miller <brad.miller@webbadger.com>
2017-11-09 22:05:33 -0700

    refactor(): reorganize data.py to read in all data sources

--- cerp/data.py
+++ cerp/data.py
@@ -2,10 +2,19 @@
     Load all the data
 """
 import json
+import os
 import pandas as pd
 
 
-def presidential_election_16():
+
+# ELECTION_DATA = {
+#     "<topic>-<year>": {
+#         "data": pd.
+#         "meta": {}
+#     }
+# }
+
+def get_voter_data():
     """
         This returns the presidental data for the 2016 election.
         Specifically a dataframe that looks like:
@@ -15,43 +24,41 @@ def presidential_election_16():
         precinctNum   votes  |   votes   | ...
         ...
     """
-    with open("cerp/static/2016-PresidentialElection.json") as file_handler:
-        data = json.load(file_handler)
+    full_data = {}
+    path = "cerp/static/microdata";
+    for file in os.listdir(path=path):
+        if file.endswith(".json"):
+            with open(path + "/" + file) as file_handler:
+                data = json.load(file_handler)
 
-    # Convert to DataFrame and select columns we care about
-    data_frame = pd.DataFrame(data['precincts'])
-    data_frame = data_frame[['precinctNumber', 'votes']]
+            # Convert to DataFrame and select columns we care about
+            data_frame = pd.DataFrame(data['precincts'])
 
-    # Iterate through dataframe to create list with proper associations
-    result = []
-    for _, row in data_frame.iterrows():
-        for dct in row['votes']:
-            dct['precinctNumber'] = row['precinctNumber']
-            result.append(dct)
+            data_frame = data_frame[['precinctNumber', 'votes']]
 
-    # Convert back to dataframe and pivot to what you wanted
-    return pd.DataFrame(result).pivot(
-        index='precinctNumber',
-        columns='candidate',
-        values='votes')
+            # Iterate through dataframe to create list with proper associations
+            result = []
+            for _, row in data_frame.iterrows():
+                for dct in row['votes']:
+                    dct['precinctNumber'] = row['precinctNumber']
+                    result.append(dct)
+            final_df = pd.DataFrame(result).pivot(
+            index='precinctNumber',
+            columns='candidate',
+            values='votes')
 
+            meta_obj = {}
+            for precinct in data['precincts']:
+                pnum = precinct['precinctNumber']
+                del precinct["votes"]
+                del precinct["precinctNumber"]
+                meta_obj[pnum] = precinct
+            full_data[data['topic'] + "-" + data['year']] = {}
+            full_data[data['topic'] + "-" + data['year']]['data'] = final_df
+            full_data[data['topic'] + "-" + data['year']]['meta'] = meta_obj    
 
-def presidential_election_16_meta():
-    """
-        This loads all the meta data about the precincts
-    """
-    with open("cerp/static/2016-PresidentialElection.json") as file_handler:
-        data = json.load(file_handler)
-
-    meta_obj = {}
-    for precinct in data['precincts']:
-        pnum = precinct['precinctNumber']
-        del precinct["votes"]
-        del precinct["precinctNumber"]
-        meta_obj[pnum] = precinct
-    return meta_obj
+    return full_data
 
 
 # Load all
-PRESIDENTIAL_ELECTION_CANADITS_16 = presidential_election_16()
-PRESIDENTIAL_ELECTION_CANADITS_16_META = presidential_election_16_meta()
+ELECTION_DATA = get_voter_data()
